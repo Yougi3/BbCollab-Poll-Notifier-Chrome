9ba3ec750905973f1886c1998016ce7b799d723e
Yougi3 <younes.aassila@outlook.fr>
2020-05-06 15:46:14 +0200

    Initial commit

---
+++ background-script.js
@@ -0,0 +1,17 @@
+var notificationId = "";
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.poll) {
+    chrome.notifications.create({
+      "type": "basic",
+      "iconUrl": chrome.extension.getURL("icons/icon-48.png"),
+      "title": "⚠️ Nouveau sondage disponible !",
+      "message": "Un nouveau sondage est disponible sur la classe virtuelle. Penses à y répondre !"
+    }, function(id) {
+      notificationId = id;
+    });
+  } else {
+    chrome.notifications.clear(notificationId);
+  }
+  sendResponse();
+});
